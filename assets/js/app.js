623b0903f1ae008b17807e4d52d9b05dab8dc487
Shawn Assour <sassour@Shawns-MacBook-Pro.local>
2017-03-15 18:04:32 -0500

    added ajax call

--- assets/js/app.js
+++ assets/js/app.js
@@ -1,11 +1,23 @@
-var key = 'AIzaSyDoWJgdorZrPzcBPAAb75fAA6h-XhDy56E'; //google maps api key
-
 function displayMap() {
+	var key = 'AIzaSyDoWJgdorZrPzcBPAAb75fAA6h-XhDy56E'; //google maps api key
 	var userInput = $("#user-input").val().trim();
 	var source = 'https://www.google.com/maps/embed/v1/search?key=' + key + '&q=' + userInput;
 	var map = $('<iframe>');
 	map.attr('src', source);
 	$("#map").append(map);
+	getRatings(key, userInput);
+}
+
+function getRatings(key, userInput) {
+	var queryURL = 'https://maps.googleapis.com/maps/api/place/textsearch/xml?query=' + userInput + '&key=' + key;
+	console.log(queryURL);
+
+	$.ajax({
+		url: queryURL,
+		method: 'GET'
+	}).done(function(response){
+		console.log(response.results);
+	});
 }
 
 
